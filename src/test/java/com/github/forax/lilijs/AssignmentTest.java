385b1613b2fa6497db957d953bfa6f3c7e01553a
forax <forax@univ-mlv.fr>
2025-03-30 10:34:53 +0200

    ?: is now implemented

--- src/test/java/com/github/forax/lilijs/AssignmentTest.java
+++ src/test/java/com/github/forax/lilijs/AssignmentTest.java
@@ -213,7 +213,7 @@ public class AssignmentTest {
         """));
   }
 
-  @Test @Disabled
+  @Test
   public void testConditionalAssignment() {
     assertEquals("true case\n", execute("""
         let condition = true;
